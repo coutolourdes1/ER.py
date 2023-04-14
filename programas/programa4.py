4d2b16fc05337ac63f02e2ff2fbe13d4edbce6e6
Enzo Cerizola <ecerizola@intermedia.com.uy>
2023-04-14 18:08:08 -0300

    1era version p4

---
+++ programas/programa4.py
@@ -0,0 +1,31 @@
+import re
+import sys
+
+
+def stringFromMatch(match):
+    return f"{match[2]} {len(match[3].split(','))}"
+    print(match)
+
+
+def prog(texto):
+
+    matches = re.findall(
+        r'{\s*("tag": ("(.+)")),\s*("patterns":\s*\[\s*((".*",{0,1}\s*))*\]),\s*("responses":\s*\[\s*((".*",{0,1}\s*))*)(.|\s)*?}', texto)
+
+    result = '\n'.join(map(stringFromMatch, matches))
+    return result
+
+
+if __name__ == '__main__':
+    entrada = sys.argv[1]  # archivo entrada (param)
+    salida = sys.argv[2]   # archivo salida (param)
+
+    f = open(entrada, 'r')  # abrir archivo entrada
+    datos = f.read()       # leer archivo entrada
+    f.close()              # cerrar archivo entrada
+
+    ret = prog(datos)      # ejecutar er
+
+    f = open(salida, 'w')  # abrir archivo salida
+    f.write(ret)           # escribir archivo salida
+    f.close()              # cerrar archivo salida
